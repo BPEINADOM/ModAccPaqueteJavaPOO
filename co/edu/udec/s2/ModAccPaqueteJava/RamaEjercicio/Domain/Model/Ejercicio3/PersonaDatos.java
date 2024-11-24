f3e920be0905c0b14c5d3cb02253c64f58b8ee92
7502410037BillyPeinadoM <bpeinadom@unicartagena.edu.co>
2024-11-24 00:03:06 -0500

    Ejercicio 3 completado

---
+++ co/edu/udec/s2/ModAccPaqueteJava/RamaEjercicio/Domain/Model/Ejercicio3/PersonaDatos.java
@@ -0,0 +1,17 @@
+package Domain.Model.Ejercicio3;
+
+public class PersonaDatos {
+
+    public static void main(String[] args) {
+
+        System.out.println("*****************7502410037******************");
+        System.out.println("********BILLY JEAN YER PEINADO MADERA********");
+        System.out.println("*********************************************");
+        
+        Persona persona = new Persona("Manuel", 15);
+
+        persona.mostrarDatos();
+
+        System.out.println("*********************************************");
+    }
+}
